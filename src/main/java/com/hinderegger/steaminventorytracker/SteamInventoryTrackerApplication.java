df2ae87a057f37d96c7f7b503ecebc04398f3676
Jaro <jaro@hinderegger.com>
2022-04-05 10:53:52 +0200

    Initial commit

---
+++ src/main/java/com/hinderegger/steaminventorytracker/SteamInventoryTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hinderegger.steaminventorytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamInventoryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamInventoryTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hinderegger/steaminventorytracker/SteamInventoryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinderegger.steaminventorytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamInventoryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
