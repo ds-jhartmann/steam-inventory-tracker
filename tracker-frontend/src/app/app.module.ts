b568ae8f307671ef4d3d6c4662eb14c12a7d037c
jrohrtmn <jaro@hinderegger.com>
2023-03-24 18:59:25 +0100

    feat: add first draft of item representation

--- tracker-frontend/src/app/app.module.ts
+++ tracker-frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ItemsComponent} from './items/items.component';
+import {FormsModule} from "@angular/forms";
+import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
